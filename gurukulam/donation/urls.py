6364fffc8af7fd34ecd485cbe477e646a52d1197
riscvsi <raghurama86@gmail.com>
2024-02-25 16:55:28 +0530

    basic page

--- gurukulam/donation/urls.py
+++ gurukulam/donation/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path, include
 from .views import home, success
 urlpatterns = [
-    path('', home, "name=home"),
-    path('success/', success, "name=success")
+    path('', home, name="home"),
+    path('success/', success, name="success")
 ]
