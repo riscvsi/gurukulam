e9187a7b869e2368fa670b70450410a16e7adeae
riscvsi <raghurama86@gmail.com>
2024-02-25 21:00:28 +0530

    models added create migrations and create fields in database

--- gurukulam/gurukulam/settings.py
+++ gurukulam/gurukulam/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'donation',
+    'donation.app.Donation',
 ]
 
 MIDDLEWARE = [
