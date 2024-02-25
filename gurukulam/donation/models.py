9616520f2dc6ce94971d35be53b7c3cbb3a1d309
riscvsi <raghurama86@gmail.com>
2024-02-25 19:57:49 +0530

    models added create migrations and create fields in database

--- gurukulam/donation/models.py
+++ gurukulam/donation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Donation(models.Model):
+    name = models.CharField(max_length=100)
+    gothram = models.CharField(max_length=100, default="Nil")
+    email = models.CharField(max_length=100)
+
+    ammount = models.CharField(max_length=100)
+    payment_id = models.CharField(max_length=100)
+    paid = models.BooleanField(default=False)
