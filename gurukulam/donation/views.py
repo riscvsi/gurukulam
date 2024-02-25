299735f75dad6eda4c2d9a91c34ce6320722ebcc
riscvsi <raghurama86@gmail.com>
2024-02-25 23:22:45 +0530

    models added create migrations and create fields in database

--- gurukulam/donation/views.py
+++ gurukulam/donation/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        ammount = request.POST.get("ammount")
+        print(name)
+        print(ammount)
     return render(request, "index.html")
 
 
