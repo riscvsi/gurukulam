1a50a411ae0f3b6dc7370cb220c6b3f7ce292a2a
riscvsi <raghurama86@gmail.com>
2024-02-26 16:16:10 +0530

    models added create migrations and create fields in database

--- gurukulam/donation/views.py
+++ gurukulam/donation/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+import razorpay
+from .models import DonationGurukulam
+
 def home(request):
     if request.method == "POST":
         name = request.POST.get("name")
         ammount = request.POST.get("ammount")
+        email = request.POST.get("email")
         print(name)
         print(ammount)
+        client = razorpay.Client(auth=("rzp_test_NqMTyxGPRznNqZ","q5lpOYcSqvHq48MdyI610EOk")
+        payment = client.order.create({'ammount': ammount, 'currency': "INR","payment_capture": '1'})
+        donateGurukul = DonationGurukulam(name=name, ammount = ammount, email = email, payment_id = payment['id'])
+        print(payment)
+        return render(request, "index.html", {'payment':payment})
     return render(request, "index.html")
 
 
 def success(request):
+    if request.method == "POST":
+        a = request.POST
+        print(a)
     return  render(request, "success.html")
